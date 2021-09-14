b9300959aae08fce63fde56d91603ba3d5a561a1
Yuri <yuri.strack@gmail.com>
2021-09-14 12:46:48 -0300

    Remove navigation bar background in detail view
    Workaround method was needed to achieve the desired effect

--- Ingresso-Challenge/Extensions/UINavigationController+Extension.swift
+++ Ingresso-Challenge/Extensions/UINavigationController+Extension.swift
@@ -8,8 +8,20 @@
 import UIKit
 
 extension UINavigationController {
-    // Remove back button text
+    
+    // Remove back button text and change navigation to transparent in detail view
     open override func viewWillLayoutSubviews() {
         navigationBar.topItem?.backButtonDisplayMode = .minimal
+        
+        // Workaround to change the navigation bar to transparent in Movies Detail View
+        // Only works in this specific projetc, not recommended solution.
+        if self.viewControllers.count > 1 {
+            navigationBar.setBackgroundImage(UIImage(), for: .default)
+            navigationBar.shadowImage = UIImage()
+        }
+        else {
+            navigationBar.setBackgroundImage(nil, for: .default)
+            navigationBar.shadowImage = nil
+        }
     }
 }
