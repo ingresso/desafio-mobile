9257d7c28a715c20984af2d5b9df8ece2c72b151
Jean Silva <jeanbarrossilva@outlook.com>
2021-12-01 20:23:56 -0300

    ui: add bare bone SoonFragment

--- ui/src/main/java/com/jeanbarrossilva/ingresso/ui/activity/MainActivity.kt
+++ ui/src/main/java/com/jeanbarrossilva/ingresso/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jeanbarrossilva.ingresso.ui
+package com.jeanbarrossilva.ingresso.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jeanbarrossilva.ingresso.ui.R
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ ui/src/main/java/com/jeanbarrossilva/ingresso/ui/fragment/SoonFragment.kt
@@ -0,0 +1,6 @@
+package com.jeanbarrossilva.ingresso.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.jeanbarrossilva.ingresso.ui.R
+
+class SoonFragment: Fragment(R.layout.fragment_soon)
