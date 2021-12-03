c33972cf7d241ea59eab267d7722128304eb0a6f
Jean Silva <jeanbarrossilva@outlook.com>
2021-12-03 18:11:15 -0300

    ui: add IngressoBottomNavigationView

--- extensions/src/main/java/com/jeanbarrossilva/ingresso/extensions/context/Context.kt
+++ extensions/src/main/java/com/jeanbarrossilva/ingresso/extensions/context/Context.kt
@@ -2,9 +2,23 @@ package com.jeanbarrossilva.ingresso.extensions.context
 
 import android.content.Context
 import android.content.res.TypedArray
+import android.graphics.Color
 import android.util.AttributeSet
+import android.util.TypedValue
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
 import androidx.annotation.StyleableRes
 import androidx.core.content.withStyledAttributes
+
+/** Gets the color that corresponds to [colorAttrRes]. **/
+@ColorInt
+fun Context.colorOf(@AttrRes colorAttrRes: Int): Int {
+    val typedArray = theme.obtainStyledAttributes(intArrayOf(colorAttrRes))
+    val color = typedArray.getColor(0, Color.TRANSPARENT)
+
+    typedArray.recycle()
+    return color
+}
 
 /** @see withStyledAttributes **/
 fun Context.withStyledAttributes(
---
+++ ui/src/main/java/com/jeanbarrossilva/ingresso/ui/view/IngressoBottomNavigationView.kt
@@ -0,0 +1,32 @@
+package com.jeanbarrossilva.ingresso.ui.view
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.util.AttributeSet
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jeanbarrossilva.ingresso.extensions.color.copy
+import com.jeanbarrossilva.ingresso.extensions.context.colorOf
+import com.jeanbarrossilva.ingresso.ui.R
+
+class IngressoBottomNavigationView: BottomNavigationView {
+    /** Short-hand for getting the theme secondary color through [colorOf]. **/
+    private val secondaryColor
+        get() = context.colorOf(R.attr.colorSecondary)
+
+    constructor(context: Context): super(context)
+
+    constructor(context: Context, attrs: AttributeSet?): super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+
+    init {
+        itemIconTintList = ColorStateList.valueOf(secondaryColor)
+        itemTextColor = ColorStateList.valueOf(secondaryColor)
+        itemRippleColor = secondaryColor
+            .let(Color::valueOf)
+            .copy(alpha = .026f)
+            .toArgb()
+            .let(ColorStateList::valueOf)
+    }
+}
