75c4965803f55059417a71491a869d796800fa56
Jean Silva <jeanbarrossilva@outlook.com>
2021-12-04 12:21:46 -0300

    fix: pass item type to MovieSearchResultAdapter

--- ui/src/main/java/com/jeanbarrossilva/ingresso/ui/adapter/MovieSearchResultAdapter.kt
+++ ui/src/main/java/com/jeanbarrossilva/ingresso/ui/adapter/MovieSearchResultAdapter.kt
@@ -1,17 +1,18 @@
 package com.jeanbarrossilva.ingresso.ui.adapter
 
+import com.jeanbarrossilva.ingresso.model.Movie
 import com.jeanbarrossilva.ingresso.ui.core.IngressoAdapter
 import com.jeanbarrossilva.ingresso.ui.databinding.ViewMovieSearchResultBinding
 import com.jeanbarrossilva.ingresso.ui.viewholder.MovieSearchResultViewHolder
 
-class MovieSearchResultAdapter: IngressoAdapter<ViewMovieSearchResultBinding, MovieSearchResultViewHolder>() {
+class MovieSearchResultAdapter: IngressoAdapter<ViewMovieSearchResultBinding, MovieSearchResultViewHolder, Movie>() {
     override val bindingClass = ViewMovieSearchResultBinding::class
-    override val items = emptyList<Any>()
+    override val items = emptyList<Movie>()
 
     override fun onCreateViewHolder(): MovieSearchResultViewHolder {
         return MovieSearchResultViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: MovieSearchResultViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MovieSearchResultViewHolder, item: Movie) {
     }
 }
