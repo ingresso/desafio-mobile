fe5902e78e9b41be7c10c726a8299996bc8db983
Jean Silva <jeanbarrossilva@outlook.com>
2021-12-04 12:26:14 -0300

    ui: show sample movies in MoviesFragment

--- ui/src/main/java/com/jeanbarrossilva/ingresso/ui/fragment/MoviesFragment.kt
+++ ui/src/main/java/com/jeanbarrossilva/ingresso/ui/fragment/MoviesFragment.kt
@@ -1,8 +1,17 @@
 package com.jeanbarrossilva.ingresso.ui.fragment
 
+import android.os.Bundle
+import android.view.View
+import com.jeanbarrossilva.ingresso.repository.Repository
+import com.jeanbarrossilva.ingresso.ui.adapter.MoviePosterAdapter
 import com.jeanbarrossilva.ingresso.ui.core.IngressoFragment
 import com.jeanbarrossilva.ingresso.ui.databinding.FragmentMoviesBinding
 
 class MoviesFragment: IngressoFragment<FragmentMoviesBinding>() {
     override val bindingClass = FragmentMoviesBinding::class
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.moviesView.adapter = MoviePosterAdapter(Repository.movies)
+    }
 }
